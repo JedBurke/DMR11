b08424b1780805823b025a3cf8b047c08eaf1180
Jed Burke <iamthefnx@gmail.com>
2018-02-28 23:53:04 +0200

    Implement word conversion class

---
+++ DMR11.Core/Helper/TitleCase.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMR11.Core.Helper
+{    
+    public class WordConverter
+    {
+        public static string ToTitleCase(string input)
+        {
+            return ToTitleCase(input, null);
+        }
+
+        public static string ToTitleCase(string input, string[] excludedWords)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new NullReferenceException();
+
+            var titleCasedString = new StringBuilder();
+
+            var splitWords = input.Split(' ');
+
+            for (var wordIndex = 0; wordIndex < splitWords.Length; wordIndex++)
+            {
+                // Drop the casing.
+                var currentWord = splitWords[wordIndex].ToLowerInvariant();
+
+                // Capitialize the first word, last word, and words which aren't excluded.
+                if (wordIndex == 0 || wordIndex == splitWords.Length -1 || (excludedWords != null && Array.IndexOf(excludedWords, currentWord) == -1))
+                {
+                    currentWord = CapitalizeWord(currentWord);
+                }
+
+                // Todo: Don't capitalize a single-lettered word which isn't I.
+
+                // Append a space to the title-cased sentence with all words except the first one.
+                if (wordIndex > 0)
+                {
+                    titleCasedString.Append(" ");
+                }
+
+                titleCasedString.Append(currentWord);
+            }
+
+            return titleCasedString.ToString();
+        }
+
+        public static string CapitalizeWord(string input)
+        {
+            string capitalizedWord = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                capitalizedWord = input;
+            }
+            else
+            {
+                if (input.Length == 1)
+                {
+                    capitalizedWord = input.ToUpperInvariant();
+                }
+                else
+                {
+                    capitalizedWord = string.Concat(input[0].ToString().ToUpperInvariant(), input.Substring(1));
+                }
+            }
+
+            return capitalizedWord;
+
+        }
+
+        public static readonly string[] ExclusionsJpn = new string[] {
+            ""
+        };
+
+        public static readonly string[] ExclusionsEng = new string[] {
+            // Articles
+            "a",
+            "an",
+            "the",
+
+            // Conjunctions
+            "and",
+
+            // Prepositions
+            "aboard",
+            "about",
+            "above",
+            "across",
+            "after",
+            "against",
+            "along",
+            "amid",
+            "among",
+            "anti",
+            "around",
+            "as",
+            "at",            
+            "before",
+            "behind",
+            "below",
+            "beneath",
+            "beside",
+            "besides",
+            "between",
+            "beyond",
+            "but",
+            "by",
+            "concerning",
+            "considering",
+            "despite",
+            "down",
+            "during",
+            "except",
+            "excepting",
+            "excluding",
+            "following",
+            "for",
+            "from",
+            "in",
+            "inside",
+            "into",
+            "like",
+            "minus",
+            "near",
+            "of",
+            "off",
+            "on",
+            "onto",
+            "opposite",
+            "outside",
+            "over",
+            "past",
+            "per",
+            "plus",
+            "regarding",
+            "round",
+            "save",
+            "since",
+            "than",
+            "through",
+            "to",
+            "toward",
+            "towards",
+            "under",
+            "underneath",
+            "unlike",
+            "until",
+            "up",
+            "upon",
+            "versus",
+            "via",
+            "with",
+            "within",
+            "without"
+        };
+
+    }
+}
---
+++ DMR11_Tests/WordConversionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DMR11.Core.Helper;
+
+namespace DMR11_Tests
+{
+    [TestClass]
+    public class WordConversionTests
+    {
+        [TestMethod]
+        public void Test_TitleCase()
+        {
+            Assert.AreEqual(
+                "Snow White and the Seven Dwarfs",
+                WordConverter.ToTitleCase(
+                    "snow white and the seven dwarfs",
+                    WordConverter.ExclusionsEng
+                )
+            );
+
+            Assert.AreEqual(
+                "Brighton on Sea",
+                WordConverter.ToTitleCase(
+                    "brighton on sea",
+                    WordConverter.ExclusionsEng
+                )
+            );
+
+            Assert.AreEqual(
+                "The Last of the Mohicans",
+                WordConverter.ToTitleCase(
+                    "the last of the mohicans",
+                    WordConverter.ExclusionsEng
+                )
+            );
+        }
+    }
+}

d1dbd3fe7041fc1bdf916712ea6964329d279738
Jed Burke <iamthefnx@gmail.com>
2018-06-28 23:12:01 +0200

    Replace Miri look-up icon

---
+++ DMR11/Dmr11BaseView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMR11
+{
+    /// <summary>
+    /// Represents the view which all DMR11 views inherit. Used to keep the layout uniform among the application.
+    /// </summary>
+    public class Dmr11BaseView
+    {
+    }
+}
